6ff02acfcbb6adf0a4684b8c4bc85686cc23e968
Sefukamil20R <Tenzilk20@gmail.com>
2024-05-28 15:53:52 +0300

    backend integrated

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-1d334/us-central1/api",
+  // localinstance of firebase function
+  // baseURL: "http://127.0.0.1:5001/clone-1d334/us-central1/api",
+  // deployed version of amazone server on render.com
+  baseURL: "https://amazone-api-deploy-pnco.onrender.com",
 });
 
 export { axiosInstance };
